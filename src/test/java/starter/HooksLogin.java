5f30d4e7701839f720705f091780be4853675b33
muhamaz <humamzaky7@gmail.com>
2023-08-13 06:46:01 +0700

    hooks setup fix

--- src/test/java/starter/HooksLogin.java
+++ src/test/java/starter/HooksLogin.java
@@ -1,4 +1,42 @@
 package starter;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import net.serenitybdd.rest.SerenityRest;
+import starter.Utils.Constants;
+
+import java.io.File;
+
 public class HooksLogin {
+
+    @Before
+    public void loginUser(){
+        File json = new File("src/test/resources/JSON/ReqBody/Authentications/Login/ValidLoginUser.json");
+        Response response= SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .post(Constants.BASE_URL+"/login");
+        JsonPath jsonPath = response.jsonPath();
+        Constants.TOKEN_USER = jsonPath.get("data.token");
+    }
+
+    @Before
+    public void loginAdmin(){
+        File json = new File("src/test/resources/JSON/ReqBody/Authentications/Login/ValidLoginAdmin.json");
+        Response response= SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .post(Constants.BASE_URL+"/login");
+        JsonPath jsonPath = response.jsonPath();
+        Constants.TOKEN_ADMIN = jsonPath.get("data.token");
+    }
+
+    @After
+    public void resetTokenUser() {
+        Constants.TOKEN_USER = null;
+        Constants.TOKEN_ADMIN = null;
+    }
 }
--- src/test/java/starter/StepDef/MiddlemanStatusCodeResponses.java
+++ src/test/java/starter/StepDef/MiddlemanStatusCodeResponses.java
@@ -1,5 +1,54 @@
 package starter.StepDef;
 
+import io.cucumber.java.en.Then;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+import starter.Middleman.MiddlemanAPI;
+
 public class MiddlemanStatusCodeResponses {
 
+    @Steps
+    MiddlemanAPI middlemanAPI;
+
+    @Then("Status code should be {int} OK")
+    public void statusCodeShouldBeOK(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} Created")
+    public void statusCodeShouldBeCreated(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} No Content")
+    public void statusCodeShouldBeNoContent(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} Unauthorized")
+    public void statusCodeShouldBeUnauthorized(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} Bad Request")
+    public void statusCodeShouldBeBadRequest(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} Not Found")
+    public void statusCodeShouldBeNotFound(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
+
+    @Then("Status code should be {int} Internal Server Error")
+    public void statusCodeShouldBeInternalServereError(int statusCode) {
+        SerenityRest.then()
+                .statusCode(statusCode);
+    }
 }
