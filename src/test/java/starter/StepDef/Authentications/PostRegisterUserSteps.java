77e12490465f8b9fb1dc6d90c30ca7823d3190f5
muhamaz <humamzaky7@gmail.com>
2023-08-14 06:19:23 +0700

    change structure folder authentications

--- src/test/java/starter/StepDef/Authentications/PostRegisterUserSteps.java
+++ src/test/java/starter/StepDef/Authentications/PostRegisterUserSteps.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.When;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
-import starter.Middleman.MiddlemanAPI;
+import starter.Middleman.AuthenticationsAPI;
 import starter.Middleman.MiddlemanResponses;
 import starter.Utils.Constants;
 
@@ -17,18 +17,18 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class PostRegisterUserSteps {
 
     @Steps
-    MiddlemanAPI middlemanAPI;
+    AuthenticationsAPI authenticationsAPI;
 
     @Given("POST Register user with JSON file {string}")
     public void postRegisterUserWithJSONFile(String jsonName) {
         File json = new File(Constants.REQ_BODY_REGISTER + ""+jsonName+"");
-        middlemanAPI.postRegisterUser(json);
+        authenticationsAPI.postRegisterUser(json);
     }
 
     @When("Send POST Register user")
     public void sendPOSTRegisterUser() {
         SerenityRest.when()
-                .post(MiddlemanAPI.POST_REGISTER_USER);
+                .post(AuthenticationsAPI.POST_REGISTER_USER);
     }
 
     @And("Response body should display register message {string}")
@@ -54,6 +54,6 @@ public class PostRegisterUserSteps {
     @When("Send POST Register user with invalid path")
     public void sendPOSTRegisterUserWithInvalidPath() {
         SerenityRest.when()
-                .post(MiddlemanAPI.POST_REGISTER_USER_INVALID_PATH);
+                .post(AuthenticationsAPI.POST_REGISTER_USER_INVALID_PATH);
     }
 }
