de7623cc10ab87fc3326555125c6c657eea10b5c
muhamaz <humamzaky7@gmail.com>
2023-08-13 12:30:01 +0700

    add login user feature test

--- src/test/java/starter/Middleman/MiddlemanResponses.java
+++ src/test/java/starter/Middleman/MiddlemanResponses.java
@@ -3,4 +3,7 @@ package starter.Middleman;
 public class MiddlemanResponses {
     public static String REG_SUCCESS_MESSAGE = "message";
     public static String REG_ERROR_MESSAGE = "message";
+
+    public static String LOGIN_SUCCESS_MESSAGE = "message";
+    public static String LOGIN_ERROR_MESSAGE = "message";
 }
